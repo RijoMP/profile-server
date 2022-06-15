088f7747214ee0bb21eb2f2bd797219b91ba22b0
rijomp <rijomp99@gmail.com>
2022-06-15 10:31:29 -0700

    added cors

--- web.js
+++ web.js
@@ -4,5 +4,9 @@ const app = express()
 app.get("/api",(req, res)=>{
     res.json({"users":["p1","p2",'p3']})
 })
+app.use(cors({
+    origin: '*'
+}));
 
 app.listen(process.env.PORT || 5000, ()=>{console.log("server statrted at 5000")})
+
