4b4167b84dd1ccfc310adb87dc3d9b27c850d543
rijomp <rijomp99@gmail.com>
2022-06-15 08:16:33 -0700

    Initial commit

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/api",(req, res)=>{
-    res.json({"users":["p1","p2",'p3']})
-})
-
-app.listen(5000, ()=>{console.log("server statrted at 5000")})
