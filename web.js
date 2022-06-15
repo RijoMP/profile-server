64ae9cd13678fa8b393883f02244773c0957efcc
rijomp <rijomp99@gmail.com>
2022-06-15 08:29:12 -0700

    Changed the port for heroku

--- web.js
+++ web.js
@@ -5,4 +5,4 @@ app.get("/api",(req, res)=>{
     res.json({"users":["p1","p2",'p3']})
 })
 
-app.listen(5000, ()=>{console.log("server statrted at 5000")})
+app.listen(process.env.PORT || 5000, ()=>{console.log("server statrted at 5000")})
